4c9d0f08e36bde638677a770cfb4b760d65650c6
mmazzini-86 <81997261+mmazzini-86@users.noreply.github.com>
2021-04-06 16:22:31 -0300

    Delete RuncCukesTest.java

--- CucumberProject/src/test/java/RuncCukesTest.java
+++
@@ -1,9 +0,0 @@
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-//con la anotacion CucumberOptions le digo donde estan los features y le indico que los pegue en donde tengo los steps
-@CucumberOptions(features = "src/test/java/features/search.feature", glue = "steps", 
-plugin = {"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:reporte/cucumber/report.html"})
-public class RuncCukesTest extends AbstractTestNGCucumberTests {
-
-}
